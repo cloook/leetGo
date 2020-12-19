45db01a9a9917f457e1228a52c9e2f746ef94cf4
zhangjiqiang <531560278@qq.com>
2020-12-19 15:48:52 +0800

    48

---
+++ 48.go
@@ -0,0 +1,12 @@
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	h := n / 2
+	for i := 0; i < h; i++ {
+		matrix[i], matrix[n-i-1] = matrix[n-i-1], matrix[i]
+	}
+	for i := 0; i < n; i++ {
+		for j := 0; j < i; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+}
